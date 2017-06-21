cae4b4a5bdcb8bcae2b959f69f3d11e57ab353a9
Rukaroa <rukaroa@gmail.com>
2017-06-21 16:21:27 -0500

    started form

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,8 +1,36 @@
 import React, {Component} from "react";
 
 class Registration extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        };
+    }
 
-	
+    handleChange(event){
+        
+    }
+
+    handleSubmit(event){
+
+    }
+
+	render(){
+        return(
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <div className="panel-title">Registration</div>
+                </div>
+                <div className="panel-body">
+                    {/*home name*/}
+                    {/*home email*/}
+                    {/*home password*/}
+                    {/*password confirm*/}
+                </div>
+            </div>
+        );
+    }
 
 }
 

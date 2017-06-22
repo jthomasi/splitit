00f66ae9ee63a5268d0f4f305a6cb3413aef6d73
Rukaroa <rukaroa@gmail.com>
2017-06-22 15:33:46 -0500

    base page started

--- src/components/Roommates.js
+++ src/components/Roommates.js
@@ -1,11 +1,32 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 class Roommates extends Component {
+    //add case for no roommates when props[] == 0
+    // show %share of bill
+    listRoomies(props){        
+        props.map((i)=>{
+            return(
+                <li>i</li>
+            );
+        });
+    }
 
 	render(){
         return(
             <div className="col-md-6">
-                
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <div className="panel-title">Roommates</div>
+                    </div>
+                    <div className="panel-body">
+                        <ul>{this.listRoomies(["test","array"])}</ul>
+                        <div className="btn-group">
+                            <div className="btn btn-default btn-lg">Add roommate
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }

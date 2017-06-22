741cf947ad83a89ec07bd228685e2f5bf5c1081e
Rukaroa <rukaroa@gmail.com>
2017-06-22 15:34:07 -0500

    routing started

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,23 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+import Registration from "./Registration";
+import Dashboard from "./Dashboard";
 
 class Main extends Component {
     render() {
         return (
         <div>
             <div className="btn-group">
-                <button className="btn btn-default btn-lg">Create Home</button>
+                <Link to="/register"><button className="btn btn-default btn-lg">Create Home</button></Link>
             </div>
             
             <div className="btn-group">
-                <button className="btn btn-default btn-lg">Sign-in Home</button>
+                <Link to="/dashboard"><button className="btn btn-default btn-lg">Sign-in Home</button></Link>
             </div>
+
+            <Route path="/register" component={Registration}/>
+            <Route path="/dashboard" component={Dashboard}/>
         </div>      
         );
     }

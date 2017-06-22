526b1411d7a4dfe2b40e3cba13619061cafdbb89
Rukaroa <rukaroa@gmail.com>
2017-06-22 15:28:27 -0500

    component added

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Roommates from "./Roommates";
+import Bills from "./Bills";
+
+const Dashboard = () => {
+    <div>
+        <Roommates/>
+        <Bills/>
+        <button>Log Out</button>
+    </div>  
+}
+
+export default Dashboard;

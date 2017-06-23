11e75837f74815100b6023e317f64d937be2da54
Rukaroa <rukaroa@gmail.com>
2017-06-23 18:01:40 -0500

    added return statement

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -4,11 +4,13 @@ import Roommates from "./Roommates";
 import Bills from "./Bills";
 
 const Dashboard = () => {
-    <div>
-        <Roommates/>
-        <Bills/>
-        <button>Log Out</button>
-    </div>  
+    return(
+        <div>
+            <Roommates/>
+            <Bills/>
+            <button>Log Out</button>
+        </div>
+    ); 
 }
 
 export default Dashboard;

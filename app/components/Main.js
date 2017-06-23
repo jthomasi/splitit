66ea3cc9d9b3da574ebb757e1a25e4d1898f58bd
Rukaroa <rukaroa@gmail.com>
2017-06-23 18:02:59 -0500

    added route

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import {Route, Link} from "react-router-dom";
 
 import Registration from "./Registration";
 import Dashboard from "./Dashboard";

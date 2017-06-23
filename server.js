57c8e5d5db18e4a20dadc603dec2be4ade5f9d47
Rukaroa <rukaroa@gmail.com>
2017-06-23 18:00:39 -0500

    reconfigured for heroku and commented out db

--- server.js
+++ server.js
@@ -1,41 +1,50 @@
+// Include Server Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-// var logger = require("morgan");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-// mongoose.Promise = Promise; ?
 
-// Initialize Express
+// schemas
+
+// Create Instance of Express
 var app = express();
-
+// Sets an initial port. We'll use this later in our listener
 var PORT = process.env.PORT || 3000;
 
-// Use morgan and body parser with our app
-// app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Make public a static dir
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
-// Database configuration with mongoose
+// -------------------------------------------------
+
+// MongoDB Configuration configuration (Change this URL to your own DB)
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Home_db");
 // var db = mongoose.connection;
 
-// Show any mongoose errors
-// db.on("error", function(error) {
-//   console.log("Mongoose Error: ", error);
+// db.on("error", function(err) {
+//   console.log("Mongoose Error: ", err);
 // });
-// // Once logged in to the db through mongoose, log a success message
+
 // db.once("open", function() {
 //   console.log("Mongoose connection successful.");
 // });
 
-// ROUTES 
+//MongoDB routes
+// -------------------------------------------------
+app.get("/", function(req, res) {
+  console.log("main page");
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 
+// -------------------------------------------------
 
-// Listen on Port 3000
+// Listener
 app.listen(PORT, function() {
-  console.log("App running on port 3000!");
+  console.log("App listening on PORT: " + PORT);
 });

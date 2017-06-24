15dedc2ce29db4deb28bed8fda6522f2d183bba5
rukaroa <rukaroa@gmail.com>
2017-06-24 12:57:09 -0500

    login component started

---
+++ app/components/Login.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+
+class Login extends Component {
+    render(){
+        return(
+            <div>
+                <div className="panel panel-default col-md-4">
+                    <div className="panel-heading">
+                        <div className="panel-title">Login</div>
+                    </div>
+                    <div className="panel-body">
+                        <form className="form-horizontal">
+                            {/*home name*/}
+                            <div className="form-group">
+                                <label className="col-sm-4 control-label">Home Name</label>
+                                <div className="col-sm-8">
+                                    <input type="text" className="form-control"/>
+                                </div>
+                            </div>
+                            {/*password*/}
+                            <div className="form-group">
+                                <label className="col-sm-4 control-label">Password</label>
+                                <div className="col-sm-8">
+                                    <input type="text" className="form-control"/>
+                                </div>
+                            </div>
+                            {/*login button*/}
+                            <div className="btn-group">
+                                <button className="btn btn-default btn-lg">Login</button>
+                            </div>
+                            {/*forgot password*/}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;

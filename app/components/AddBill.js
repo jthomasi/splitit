b375fe1c8a99445fb1139166d45e28b7ab72af62
Rukaroa <rukaroa@gmail.com>
2017-06-27 15:47:35 -0500

    added states for form. to add visibility on submit?

--- app/components/AddBill.js
+++ app/components/AddBill.js
@@ -1,7 +1,33 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
 
 class AddBills extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            billName: "",
+            billCost: 0,
+            dueDate: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this); 
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log("bill submitted");
+
+        // send data to Bills component and database
+        // this.props.updateBill(this.state.billName, this.state.billCost, this.state.dueDate);  
+    }
+
+    handleChange(event){
+        console.log("bill value change");
+
+        let newState = {};
+        newState[event.target.id] = event.target.value;
+        this.setState(newState);
+    }
+
     render(){
         return(
             <div>
@@ -16,21 +42,33 @@ class AddBills extends Component{
                                 <div className="form-group">
                                     <label className="col-sm-4 control-label">Bill Name</label>
                                     <div className="col-sm-8">
-                                        <input type="text" className="form-control"/>
+                                        <input type="text"
+                                        value={this.state.billName}
+                                        onChange={this.handleChange}
+                                        id="billName"
+                                        className="form-control"/>
                                     </div>
                                 </div>
                                 {/*bill cost*/}
                                 <div className="form-group">
                                     <label className="col-sm-4 control-label">Bill Cost</label>
                                     <div className="col-sm-8">
-                                        <input type="text" className="form-control"/>
+                                        <input type="number"
+                                        value={this.state.billCost}                                        
+                                        onChange={this.handleChange}
+                                        id="billCost"
+                                        className="form-control"/>
                                     </div>
                                 </div>
                                 {/*due date*/}
                                 <div className="form-group">
                                     <label className="col-sm-4 control-label">Due Date</label>
                                     <div className="col-sm-8">
-                                        <input type="text" className="form-control"/>
+                                        <input type="text" 
+                                        value={this.state.dueDate}                                        
+                                        onChange={this.handleChange}
+                                        id="dueDate"
+                                        className="form-control"/>
                                     </div>
                                 </div>
                             </form>

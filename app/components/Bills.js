ca1ee91f23c0604689f1e616d2210ac04aa3e136
Rukaroa <rukaroa@gmail.com>
2017-06-26 20:19:11 -0500

    conditional rendering added

--- app/components/Bills.js
+++ app/components/Bills.js
@@ -6,14 +6,43 @@ import AddBill from "./AddBill";
 // import {routes, Subroutes} from "../config/routes";
 
 class Bills extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          isVisible: false  
+        };
+        this.addBill = this.addBill.bind(this);
+        this.listBills = this.listBills.bind(this);
+        this.showAdd = this.showAdd.bind(this);
+    }
     //add case for no bills when props[] == 0
     //display bill name, bill cost, and bill total
+    
+    //conditionally render add scene after clicking on AddBill
+    //clicking button will show Addbill/cancel
+    //addbill fields may be retained; to solve
     listBills(props){
         props.map((i)=>{
             return(
                 <li>i</li>
             );
         });
+    }
+
+    addBill() {
+        this.setState({isVisible: !this.state.isVisible});
+    }
+
+    showAdd() {
+        console.log("showAdd");
+        if (this.state.isVisible) {
+            return(
+                <AddBill/>
+            );
+        } else {
+            console.log("no render");
+            return null;
+        }
     }
 
 	render(){
@@ -26,13 +55,12 @@ class Bills extends Component {
                     <div className="panel-body">
                         <ul>{this.listBills(["test","array"])}</ul>
                         <div className="btn-group">
-                           <Link to="/addbill">
-                            <button className="btn btn-default btn-lg">Add bill</button>
-                           </Link>
+                            <button onClick={this.addBill} className="btn btn-default btn-lg">Add bill</button>
                         </div>
                     </div>
                 </div>
-                <Route path="/addbill" component={AddBill}/>
+                {/*conditionally render add*/}
+                {this.showAdd()}
             </div>
         );
     }

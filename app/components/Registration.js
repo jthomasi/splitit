d2e694c7071732a931bbc078b10ff422b1726153
rukaroa <rukaroa@gmail.com>
2017-06-27 20:45:12 -0500

    panel heaidng fix

--- app/components/Registration.js
+++ app/components/Registration.js
@@ -32,61 +32,63 @@ class Registration extends Component {
         return(
             <div>
                 <div className="col-md-4"></div>
-                <div className="panel panel-default col-md-4">
-                    <div className="panel-heading">
-                        <div className="panel-title">Registration</div>
-                    </div>
-                    <div className="panel-body">             
-                        <form className="form-horizontal" action="/addhome" method="post">
-                            {/*home name*/}
-                            <div className="form-group">
-                                <label className="col-sm-4 control-label">Home Name</label>
-                                <div className="col-sm-8">
-                                    <input type="text"
-                                    value={this.state.homeName}
-                                    onChange={this.handleChange}
-                                    id="homeName"
-                                    className="form-control"/>
+                <div className="col-md-4">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <div className="panel-title">Registration</div>
+                        </div>
+                        <div className="panel-body">             
+                            <form className="form-horizontal" action="/addhome" method="post">
+                                {/*home name*/}
+                                <div className="form-group">
+                                    <label className="col-sm-4 control-label">Home Name</label>
+                                    <div className="col-sm-8">
+                                        <input type="text"
+                                        value={this.state.homeName}
+                                        onChange={this.handleChange}
+                                        id="homeName"
+                                        className="form-control"/>
+                                    </div>
                                 </div>
-                            </div>
-                            {/*home email*/}
-                            <div className="form-group">
-                                <label className="col-sm-4 control-label">Email</label>
-                                <div className="col-sm-8">
-                                    <input type="email"
-                                    value={this.state.homeEmail}
-                                    onChange={this.handleChange}
-                                    id="homeEmail"
-                                    className="form-control"/>
+                                {/*home email*/}
+                                <div className="form-group">
+                                    <label className="col-sm-4 control-label">Email</label>
+                                    <div className="col-sm-8">
+                                        <input type="email"
+                                        value={this.state.homeEmail}
+                                        onChange={this.handleChange}
+                                        id="homeEmail"
+                                        className="form-control"/>
+                                    </div>
                                 </div>
-                            </div>
-                            {/*home password*/}
-                            <div className="form-group">
-                                <label className="col-sm-4 control-label">Password</label>
-                                <div className="col-sm-8">
-                                    <input type="password"
-                                    value={this.state.password}
-                                    onChange={this.handleChange}
-                                    id="password"
-                                    className="form-control"/>
+                                {/*home password*/}
+                                <div className="form-group">
+                                    <label className="col-sm-4 control-label">Password</label>
+                                    <div className="col-sm-8">
+                                        <input type="password"
+                                        value={this.state.password}
+                                        onChange={this.handleChange}
+                                        id="password"
+                                        className="form-control"/>
+                                    </div>
                                 </div>
-                            </div>
-                            {/*password confirm*/}
-                            <div className="form-group">
-                                <label className="col-sm-4 control-label">Confirm Password</label>
-                                <div className="col-sm-8">
-                                    <input type="password"
-                                    value={this.state.passwordConfirm}
-                                    onChange={this.handleChange}
-                                    id="passwordConfirm"
-                                    className="form-control"/>
+                                {/*password confirm*/}
+                                <div className="form-group">
+                                    <label className="col-sm-4 control-label">Confirm Password</label>
+                                    <div className="col-sm-8">
+                                        <input type="password"
+                                        value={this.state.passwordConfirm}
+                                        onChange={this.handleChange}
+                                        id="passwordConfirm"
+                                        className="form-control"/>
+                                    </div>
                                 </div>
-                            </div>
-                            {/*submit*/}
-                            <div className="btn-group">
-                                    <button className="btn btn-default btn-lg">Submit</button>
-                             </div>
-                        </form>                    
+                                {/*submit*/}
+                                <div className="btn-group">
+                                        <button className="btn btn-default btn-lg">Submit</button>
+                                </div>
+                            </form>                    
+                        </div>
                     </div>
                 </div>
                 <div className="col-md-4"></div>

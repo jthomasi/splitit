8bdab40fb95df5ff458325d69272f09e3634c6c5
Rukaroa <rukaroa@gmail.com>
2017-06-28 19:33:15 -0500

    merged

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,27 +1,59 @@
 import React, {Component} from "react";
-import {Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 
 import Registration from "./Registration";
 import Dashboard from "./Dashboard";
 
-// import {routes, Subroutes} from "../config/routes";
-
 class Main extends Component {
     //handle logins and registration
-    render() {
-        return (
-        <div>
-            <div className="btn-group">
-                <Link to="/register"><button className="btn btn-default btn-lg">Create Home</button></Link>
-            </div>
-            
-            <div className="btn-group">
-                <Link to="/dashboard"><button className="btn btn-default btn-lg">Sign-in Home</button></Link>
-            </div>
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoggedIn: false
+        };
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+    //change login status
+    handleLogin(event){
+        event.preventDefault();
+        //if authentication is true; to do later
+        this.setState({isLoggedIn: true});
+    }
 
-            <Route path="/register" component={Registration}/>
-            <Route path="/dashboard" component={Dashboard}/>
-        </div>      
+    render() {
+        let isLoggedIn = this.state.isLoggedIn;
+        console.log(`isLoggedIn = {isLoggedIn}`);
+
+        return (
+            <Router>
+                <div className="container">
+                    <div className="jumbotron">
+                        <h2>Splitit</h2>
+                        <p>Splitting the rent and bills made easy(ier).</p>
+                    </div>
+
+                    <div className="btn-group">
+                        <Link to="/register"><button className="btn btn-default btn-lg">Create Home</button></Link>
+                    </div>
+                    
+                    <div className="btn-group">
+                        <Link to="/dashboard"><button className="btn btn-default btn-lg">Sign-in Home</button></Link>
+                    </div>
+
+                    {/*original routing*/}
+                    {/*<Route path="/register" component={Registration}/>
+                    <Route path="/dashboard" component={Dashboard}/>*/}
+
+                    {/*trying to pass props to children*/}
+                    {/*see https://reacttraining.com/react-router/web/api/Route/render-func*/}
+                    <Route path="/register" render={()=>
+                        <Registration isLoggedIn={isLoggedIn}/>
+                    }/>
+                    <Route path="/dashboard" render={()=>
+                        <Dashboard isLoggedIn={isLoggedIn}/>
+                    }/>
+                </div>
+            </Router>      
         );
     }
 }

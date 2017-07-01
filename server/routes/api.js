36e80c490285053cc90747d95fd09d1a4b79d2de
Jesse Thomas <jayayetea@gmail.com>
2017-07-01 11:40:22 -0500

    added comments

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,6 +6,7 @@ router.get('/dashboard', (req, res) => {
   res.status(200).json({
     message: "You're authorized to see this secret message."
   });
+  // needs a route to grab related information from bills and roommates model
 });
 
 module.exports = router;

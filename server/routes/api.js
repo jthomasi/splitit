9806560b32fecd58695fe7452ce7cde71179ee0b
Jesse Thomas <jayayetea@gmail.com>
2017-07-04 17:09:23 -0500

    added console.log check for testing in server/routes/api.js for passing data to dashboard after login

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const Home = require('mongoose').model('Home');
+const Roommate = require('mongoose').model('Roommate');
+const Bill = require('mongoose').model('Bill');
+const passport = require('passport');
+
 const router = new express.Router();
 
+// this is where we will passing data from the login to the dashboard
 router.get('/dashboard', (req, res) => {
+  console.log("dashboard data: "+JSON.stringify(req.body, null, 4));
   res.status(200).json({
     message: "You're authorized to see this secret message."
   });

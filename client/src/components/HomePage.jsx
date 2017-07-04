38b2c3d409b2fa6fce127a9cfd1ad6b3d8c23c68
Rukaroa <rukaroa@gmail.com>
2017-07-04 16:38:06 -0500

    front page desgin start. non-working image for some reason

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Card, CardTitle } from 'material-ui/Card';
+import { Card, CardTitle, CardMedia, CardText } from 'material-ui/Card';
 
 
 const HomePage = () => (
   <Card className="container">
     <CardTitle title="Splitit User Authentication" subtitle="This is the home page." />
+    <CardMedia>
+      <img src="../../dist/img/split-rent.jpg" alt="front"/>
+    </CardMedia>
+    <CardText>
+      <p>Split the bill with your roommates, easy(ier)</p>
+    </CardText>
   </Card>
 );
 

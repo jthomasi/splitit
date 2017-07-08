7528e4981f5d0c70d8c1caf35dbd396130adc879
Rukaroa <rukaroa@gmail.com>
2017-07-08 08:54:58 -0500

    moved card title

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -4,8 +4,11 @@ import { Card, CardTitle, CardMedia, CardText } from 'material-ui/Card';
 
 const HomePage = () => (
   <Card className="container">
-    <CardTitle title="Welcome to Splitit!" subtitle="Split the bill with your roommates, easy(ier)" />
-    <CardMedia>
+    <CardMedia
+      overlay={
+        <CardTitle title="Welcome to Splitit!" subtitle="Split the bill with your roommates, easier"/>
+      }
+    >
       <img src={require("../../dist/img/splitrent.jpg")} alt="front"/>
     </CardMedia>
   </Card>

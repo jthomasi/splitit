d11992e07477d129eb0db193154b08e9bc3f933d
Rukaroa <rukaroa@gmail.com>
2017-07-12 19:07:20 -0500

    added github link to nav

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -2,13 +2,22 @@ import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import Auth from '../modules/Auth';
 
+import RaisedButton from 'material-ui/RaisedButton';
 
 const Base = ({ children }) => (
   <div>
     <div className="top-bar container">
       <div className="top-bar-left">
         <IndexLink to="/">Splitit</IndexLink>
+        <RaisedButton
+          href="https://github.com/jthomasi/splitit"
+          target="_blank"
+          label="Github Link"
+          primary={true}
+        />
       </div>
+
+      
 
       {Auth.isUserAuthenticated() ? (
         <div className="top-bar-right">
